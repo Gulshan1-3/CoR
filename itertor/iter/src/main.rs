7381bc991c8a6f795af8d4f67ba9d2bdf933eab4
Gulshan1-3 <ankitstar955@gmail.com>
2024-04-07 22:31:22 +0530

    2nd

--- itertor/iter/src/main.rs
+++ itertor/iter/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let mut iter = vec!["a","b","c"].into_iter(); //Creates a consuming iterator, that is, one that moves each value out of the vector (from start to end). The vector cannot be used after calling this.
+    while let Some(e) = iter.next(){
+
+
+    }
 }
